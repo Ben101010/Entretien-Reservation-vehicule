bf29d0e86d98feaa51422d7c8a8c0710a210bc78
ben101010 <benjaminjunioratanda10@gmail.com>
2021-07-28 20:12:34 +0000

    firstCommit

--- resources/views/layouts/TableauDeBord.blade.php
+++ resources/views/layouts/TableauDeBord.blade.php
@@ -237,7 +237,7 @@
 						<div class="panel panel-default card-view">
 							<div class="panel-heading">
 								<div class="pull-left">
-									<h6 class="panel-title txt-dark">project status</h6>
+									<h6 class="panel-title txt-dark">Status des Projets</h6>
 								</div>
 								<div class="clearfix"></div>
 							</div>
@@ -248,68 +248,61 @@
 										  <table class="table table-hover table-bordered mb-0">
 											<thead>
 											  <tr>
-												<th>Task</th>
-												<th>Progress</th>
-												<th>Deadline</th>
+												<th>Évènements</th>
+												<th>Progression</th>
+
 												</tr>
 											</thead>
 											<tbody>
+
 											  <tr>
 												<td>CMVM Digitisation of paper records</td>
 												<td><div class="progress progress-xs mb-0 ">
 													<div class="progress-bar progress-bar-danger" style="width: 35%"></div>
 												  </div></td>
-												<td>Jan 18, 2017</td>
-
 											  </tr>
+
 											  <tr>
 												<td>Data management plans</td>
 												<td><div class="progress progress-xs mb-0 ">
 													<div class="progress-bar progress-bar-warning" style="width: 50%"></div>
 												  </div></td>
-												<td>Dec 1, 2016</td>
-
 											  </tr>
+
 											  <tr>
 												<td>REF readiness</td>
 												<td><div class="progress progress-xs mb-0 ">
 													<div class="progress-bar progress-bar-success" style="width: 100%"></div>
 												  </div></td>
-												<td>Nov 12, 2016</td>
-
 											  </tr>
+
 											  <tr>
 												<td>Storage Strategy</td>
 												<td><div class="progress progress-xs mb-0 ">
 													<div class="progress-bar progress-bar-primary" style="width: 70%"></div>
 												  </div></td>
-												<td>Oct 9, 2016</td>
-
 											  </tr>
+
 											  <tr>
 												<td>Network Infrastructure strategy</td>
 												<td><div class="progress progress-xs mb-0 ">
 													<div class="progress-bar progress-bar-primary" style="width: 85%"></div>
 												  </div></td>
-												<td>Sept 2, 2016</td>
+											 </tr>
 
-											  </tr>
 											  <tr>
 												<td>Flexible Server hosting</td>
 												<td><div class="progress progress-xs mb-0 ">
 													<div class="progress-bar progress-bar-warning" style="width: 50%"></div>
 												  </div></td>
-												<td>August 11, 2015</td>
-
 											  </tr>
+
 											   <tr>
 												<td>Virtual Desktop software access</td>
 												<td><div class="progress progress-xs mb-0 ">
 													<div class="progress-bar progress-bar-warning" style="width: 50%"></div>
 												  </div></td>
-												<td>June 11, 2016</td>
-
-											  </tr>
+											    </tr>
 											</tbody>
 										  </table>
 										</div>
